bca594551a91b82154766cea54f53f66be1e35f8
hub33k <hub33k@yahoo.com>
2022-04-04 19:32:42 +0200

    fix: fix typo

--- pages/api/vote.ts
+++ pages/api/vote.ts
@@ -5,8 +5,8 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IResponse>,
 ) {
-  const emailAdress: string | string[] = req.query.email;
+  const emailAddress: string | string[] = req.query.email;
   const vote: string | string[] = req.query.vote;
   res.status(200).redirect('http://localhost:3000/thanks');
-  console.log(`User: ${emailAdress} gave us: ${vote}`);
+  console.log(`User: ${emailAddress} gave us: ${vote}`);
 }
