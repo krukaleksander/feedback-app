c3bb025e62fea4d3adae24a14da9ff504c9b6282
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-04 13:29:23 +0200

    refactor: move generate html to hylpers/generateHTML

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -3,41 +3,14 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { IResponse } from "../../interface/IResponse";
 const nodemailer = require("nodemailer");
 const sgTransport = require("nodemailer-sendgrid");
-import { compile } from "handlebars";
-import mjml2html from "mjml";
+import { generateHTML } from "../../helpers/generateHTML";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IResponse>
 ) {
   const emailAdress: string = req.body.email;
-
-  const template = compile(`
-<mjml>
-  <mj-body>
-    <mj-section>
-      <mj-column>
-        <mj-text font-size="20px" color="#F45E43" font-family="helvetica" align="center">ELP Bootcamp</mj-text>
-
-        <mj-divider border-color="#F45E43"></mj-divider>
-
-        <mj-text font-size="20px" color="#F45E43" font-family="helvetica">Rate our bootcamp</mj-text>
-
-        <mj-text align="center" font-size="16px" color="#F45E43">
-          <a href="http://localhost:3000/api/vote/?email=${emailAdress}&vote=1" color="#F45E43">1</a>
-          <a href="http://localhost:3000/api/vote/?email=${emailAdress}&vote=2" color="#F45E43">2</a>
-          <a href="http://localhost:3000/api/vote/?email=${emailAdress}&vote=3" color="#F45E43">3</a>
-          <a href="http://localhost:3000/api/vote/?email=${emailAdress}&vote=4" color="#F45E43">4</a>
-          <a href="http://localhost:3000/api/vote/?email=${emailAdress}&vote=5" color="#F45E43">5</a>
-        </mj-text>
-      </mj-column>
-    </mj-section>
-  </mj-body>
-</mjml>
-`);
-  const mjml = template(emailAdress);
-  const html = mjml2html(mjml);
-  console.log(html);
+  const generatedHTML = generateHTML(emailAdress);
   try {
     const options = {
       apiKey: process.env.API_KEY,
@@ -48,7 +21,7 @@ export default async function handler(
       to: emailAdress,
       subject: "Hello ✔",
       text: "Hello world?",
-      html: html.html,
+      html: generatedHTML,
     });
     res.status(202).json({
       statusCode: 202,
