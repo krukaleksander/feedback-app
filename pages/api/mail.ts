cdde697c36a0a3ffcf3e80305372298f057a4a29
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-04 10:05:02 +0200

    feat: import nodemailer, sgTransport; get emailAdress from req.body

--- pages/api/mail.ts
+++ pages/api/mail.ts
@@ -1,10 +1,13 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { IResponse } from "../../interface/IResponse";
+const nodemailer = require("nodemailer");
+const sgTransport = require("nodemailer-sendgrid-transport");
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<IResponse>
 ) {
+  const emailAdress = req.body.email;
+
   res.status(250).json({ statusCode: 250, msg: "Email sent successfully" });
 }
