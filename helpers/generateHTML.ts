76afa17e04184e2210596b6379bd9c6b5be2369f
hub33k <hub33k@yahoo.com>
2022-04-04 14:11:42 +0200

    feat: introduce new email template (two rates)

--- helpers/generateHTML.ts
+++ helpers/generateHTML.ts
@@ -3,25 +3,76 @@ import mjml2html from "mjml";
 export function generateHTML(mail: string) {
   const template = compile(`
 <mjml>
-  <mj-body>
-    <mj-section>
-      <mj-column>
-        <mj-text font-size="20px" color="#F45E43" font-family="helvetica" align="center">ELP Bootcamp</mj-text>
+    <mj-body>
+        <mj-section>
+            <mj-column>
+                <mj-text font-size="20px" color="#F45E43" font-family="helvetica" align="center">ELP Bootcamp</mj-text>
 
-        <mj-divider border-color="#F45E43"></mj-divider>
+                <mj-divider border-color="#F45E43"></mj-divider>
 
-        <mj-text font-size="20px" color="#F45E43" font-family="helvetica">Rate our bootcamp</mj-text>
+                <mj-text font-size="20px" color="#F45E43" font-family="helvetica">Rate our bootcamp</mj-text>
 
-        <mj-text align="center" font-size="16px" color="#F45E43">
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=1" color="#F45E43">1</a>
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=2" color="#F45E43">2</a>
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=3" color="#F45E43">3</a>
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=4" color="#F45E43">4</a>
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=5" color="#F45E43">5</a>
-        </mj-text>
-      </mj-column>
-    </mj-section>
-  </mj-body>
+                <mj-text>
+                    <form action="http://localhost:3000/api/vote" method="GET">
+                        <mj-text align="center" font-size="16px" color="#F45E43">Rate workshop</mj-text>
+
+                        <div>
+                            <input type="radio" value="1" id="rateWorkshop-1" name="rateWorkshop"/>
+                            <label for="rateWorkshop-1">1</label>
+                        </div>
+                        <div>
+                            <input type="radio" value="2" id="rateWorkshop-2" name="rateWorkshop"/>
+                            <label for="rateWorkshop-2">2</label>
+                        </div>
+                        <div>
+                            <input type="radio" value="3" id="rateWorkshop-3" name="rateWorkshop"/>
+                            <label for="rateWorkshop-3">3</label>
+                        </div>
+                        <div>
+                            <input type="radio" value="4" id="rateWorkshop-4" name="rateWorkshop"/>
+                            <label for="rateWorkshop-4">4</label>
+                        </div>
+                        <div>
+                            <input type="radio" value="5" id="rateWorkshop-5" name="rateWorkshop"/>
+                            <label for="rateWorkshop-5">5</label>
+                        </div>
+
+                        <br>
+                        <br>
+
+                        <mj-text align="center" font-size="16px" color="#F45E43">Rate Michał</mj-text>
+
+
+                        <div>
+                            <input type="radio" value="1" id="rateMichal-1" name="rateMichal"/>
+                            <label for="rateMichal-1">1</label>
+                        </div>
+                        <div>
+                            <input type="radio" value="2" id="rateMichal-2" name="rateMichal"/>
+                            <label for="rateMichal-2">2</label>
+                        </div>
+                        <div>
+                            <input type="radio" value="3" id="rateMichal-3" name="rateMichal"/>
+                            <label for="rateMichal-3">3</label>
+                        </div>
+                        <div>
+                            <input type="radio" value="4" id="rateMichal-4" name="rateMichal"/>
+                            <label for="rateMichal-4">4</label>
+                        </div>
+                        <div>
+                            <input type="radio" value="5" id="rateMichal-5" name="rateMichal"/>
+                            <label for="rateMichal-5">5</label>
+                        </div>
+
+                        <br>
+                        <br>
+
+                        <input type="submit" value="Submit">
+                    </form>
+                </mj-text>
+            </mj-column>
+        </mj-section>
+    </mj-body>
 </mjml>
 `);
   const mjml = template(mail);
