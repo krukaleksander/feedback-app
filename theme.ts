1e0707ca5c2b1b726dc69678a8a80b80dee93e7e
ann-kar <anna.paulina.karpiuk@gmail.com>
2022-04-05 10:27:22 +0200

    refactor: formatting

--- theme.ts
+++ theme.ts
@@ -1,23 +1,22 @@
-import { createTheme } from '@mui/material/styles';
+import { createTheme } from "@mui/material/styles";
 
 let theme = createTheme({
   palette: {
     primary: {
-      main: '#5de6de'
+      main: "#5de6de",
     },
     secondary: {
-      main: '#b58ecc',
+      main: "#b58ecc",
     },
     info: {
-      main: '#5de6de'
+      main: "#5de6de",
     },
     success: {
-      main: '#A7F89D'
+      main: "#A7F89D",
     },
     error: {
-      main: '#B80073'
-    }
-
+      main: "#B80073",
+    },
   },
 });
 
@@ -27,28 +26,28 @@ theme = createTheme(theme, {
     fontWeightLight: 300,
     h1: {
       fontWeight: 700,
-      fontSize: '2.5rem',
-      color: theme.palette.primary.main
+      fontSize: "2.5rem",
+      color: theme.palette.primary.main,
     },
     h2: {
       fontWeight: 200,
-      fontSize: '1.1rem',
+      fontSize: "1.1rem",
       color: theme.palette.primary.dark,
     },
     h3: {
       fontWeight: 700,
       fontSize: 30,
-      color: theme.palette.primary.main
+      color: theme.palette.primary.main,
     },
     h5: {
       fontWeight: 300,
-      fontSize: '1.125rem',
-      color: theme.palette.secondary.main
+      fontSize: "1.125rem",
+      color: theme.palette.secondary.main,
     },
     h6: {
-      fontSize: '0.9rem',
+      fontSize: "0.9rem",
       fontWeight: 300
-    }
+    },
   },
   palette: {
     info: {
@@ -57,33 +56,28 @@ theme = createTheme(theme, {
   },
   components: {
     MuiButton: {
-      defaultProps: {
-
-      },
+      defaultProps: {},
       styleOverrides: {
         root: {
-          border: '1px solid #fff',
+          border: "1px solid #fff",
           borderColor: theme.palette.info.main,
           background: `linear-gradient(165deg, ${theme.palette.primary.main},${theme.palette.success.main})`,
-          color: '#fff',
-          fontSize: '1.05rem',
-          borderRadius: '0.5rem',
-          '&:hover': {
+          color: "#fff",
+          fontSize: "1.05rem",
+          borderRadius: "0.5rem",
+          "&:hover": {
             boxShadow: `0 0 12px ${theme.palette.primary.main}`,
-        },
-
+          },
         },
       },
     },
     MuiInput: {
       styleOverrides: {
-
         height: "5rem",
         minWidth: "40rem",
-         }
-    }
-  }
+      },
+    },
+  },
 });
-
 
 export { theme };
