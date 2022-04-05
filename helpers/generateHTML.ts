6156f5fd4a4017e491235daebc0f94ab2a253a7c
hub33k <hub33k@yahoo.com>
2022-04-05 09:25:03 +0200

    feat: update email template

--- helpers/generateHTML.ts
+++ helpers/generateHTML.ts
@@ -3,21 +3,33 @@ import mjml2html from "mjml";
 export function generateHTML(mail: string) {
   const template = compile(`
 <mjml>
+  <mj-head>
+    <mj-style>
+      .vote-link {
+        display: inline-block;
+        font-size: 26px;
+        color: #F45E43;
+        padding: 16px;
+        text-decoration: none;
+      }
+      .vote-link:hover {
+        color: #fff;
+        background-color: #F45E43;
+      }
+    </mj-style>
+  </mj-head>
   <mj-body>
     <mj-section>
       <mj-column>
         <mj-text font-size="20px" color="#F45E43" font-family="helvetica" align="center">ELP Bootcamp</mj-text>
-
         <mj-divider border-color="#F45E43"></mj-divider>
-
         <mj-text font-size="20px" color="#F45E43" font-family="helvetica">Rate our bootcamp</mj-text>
-
         <mj-text align="center" font-size="16px" color="#F45E43">
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=1" color="#F45E43">1</a>
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=2" color="#F45E43">2</a>
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=3" color="#F45E43">3</a>
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=4" color="#F45E43">4</a>
-          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=5" color="#F45E43">5</a>
+          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=1" class="vote-link">1</a>
+          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=2" class="vote-link">2</a>
+          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=3" class="vote-link">3</a>
+          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=4" class="vote-link">4</a>
+          <a href="http://localhost:3000/api/vote/?email=${mail}&vote=5" class="vote-link">5</a>
         </mj-text>
       </mj-column>
     </mj-section>
