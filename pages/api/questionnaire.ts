fd01ec5aaee4beb65ce3c535f88c5a12b412b05e
Aleksander Kruk <kruk.aleksander@hotmail.com>
2022-04-06 10:05:38 +0200

    feat: create endpoint /api/questionnaire

--- pages/api/questionnaire.ts
+++ pages/api/questionnaire.ts
@@ -1,6 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { Request } from "../../dtos/request";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
-) {}
+) {
+  const data = req.body;
+  const request = new Request();
+  request.email = data.email;
+  request.answers = data.answers;
+}
